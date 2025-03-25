addc769d737b495731c2808eb04249ff9af546b1
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2025-03-25 04:05:56 +0000

    add server config variables to settings class

--- app/config.py
+++ app/config.py
@@ -1,11 +1,22 @@
+import os
 from pathlib import Path
 from pydantic_settings import BaseSettings
+from pydantic import Field
 from functools import lru_cache
 
 class Settings(BaseSettings):
     base_dir: Path = Path(__file__).resolve().parent 
     template_dir: Path = Path(__file__).resolve().parent / "templates"
     static_dir: Path = Path(__file__).resolve().parent / "static"
+    secret_key: str = Field(..., env='SECRET_KEY')
+    db_name: str = Field(...,env='DB_NAME')
+    db_server: str = Field(...,env='DB_SERVER')
+    db_username: str = Field(..., env='DB_USERNAME')
+    db_password: str = Field(..., env='DB_PASSWORD')
+    db_port: int = Field(...,env='DB_PORT')
+
+    class Config:
+        env_file='.env'
 
 @lru_cache
 def get_settings():
