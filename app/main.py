24b86710f8e576e2f1e465bfbe727c68c6095018
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2025-04-04 02:57:26 -0400

    add authentication middleware and handle unathenticated and authenticated users

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from starlette.middleware.authentication import AuthenticationMiddleware
 
-from . import config, shortcuts, models, database
+from . import config, shortcuts, backends
 from .routers.auth import users
 
 
@@ -12,10 +13,15 @@ settings = config.get_settings()
 
 app = FastAPI()
 
+app.add_middleware(AuthenticationMiddleware, backend=backends.JWTCookieBackend())
 app.mount("/static", StaticFiles(directory=str(settings.static_dir)), name="static")
 app.include_router(users.router)
 
 
 @app.get("/")
 def home_page(request: Request):
-    return shortcuts.render(request, "unauth.html", {}, status_code=200)
+
+    if request.user.is_authenticated:
+        return shortcuts.render(request, "dashboard.html", {}, status_code=200)
+    else:
+        return shortcuts.render(request, "unauth.html", {}, status_code=200)
