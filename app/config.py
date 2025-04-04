03cbffa09f8875561f75c3dab27526fedc00912a
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2025-04-04 00:34:48 -0400

    add jwt_algorithm and secret_key env variables

--- app/config.py
+++ app/config.py
@@ -4,19 +4,23 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 from functools import lru_cache
 
+
 class Settings(BaseSettings):
-    base_dir: Path = Path(__file__).resolve().parent 
+    base_dir: Path = Path(__file__).resolve().parent
     template_dir: Path = Path(__file__).resolve().parent / "templates"
     static_dir: Path = Path(__file__).resolve().parent / "static"
-    secret_key: str = Field(..., env='SECRET_KEY')
-    db_name: str = Field(...,env='DB_NAME')
-    db_server: str = Field(...,env='DB_SERVER')
-    db_username: str = Field(..., env='DB_USERNAME')
-    db_password: str = Field(..., env='DB_PASSWORD')
-    db_port: int = Field(...,env='DB_PORT')
+    secret_key: str = Field(..., env="SECRET_KEY")
+    jwt_algorithm: str = Field(default="HS256", env="JWT_ALGORITHM")
+    token_expire: int = Field(default=86400, env="TOKEN_EXPIRE")
+    db_name: str = Field(..., env="DB_NAME")
+    db_server: str = Field(..., env="DB_SERVER")
+    db_username: str = Field(..., env="DB_USERNAME")
+    db_password: str = Field(..., env="DB_PASSWORD")
+    db_port: int = Field(..., env="DB_PORT")
 
     class Config:
-        env_file='.env'
+        env_file = ".env"
+
 
 @lru_cache
 def get_settings():
