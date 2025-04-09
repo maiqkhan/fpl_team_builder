e25e2110d3072789dc8be6f70ea2f7a32cfc0079
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2025-04-09 00:44:24 -0400

    add tests for bad account credentials on signup

--- tests/test_auth_signup_post_requests.py
+++ tests/test_auth_signup_post_requests.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 from app.main import app
+import json
 from app.database import get_session
 import pytest
 
@@ -34,7 +35,7 @@ def client_fixture(session: Session):
 
 
 def test_successful_signup_post_request(client: TestClient):
-    """Test Succesfull POST Request to signup page"""
+    """Test Succesful POST Request to signup page"""
     response = client.post(
         "/signup",
         data={
@@ -42,6 +43,141 @@ def test_successful_signup_post_request(client: TestClient):
             "password": "Abcd123!",
             "reconfirmPassword": "Abcd123!",
         },
+        follow_redirects=False,
     )
-    print(response.status_code)
-    assert response.status_code == 200
+
+    assert response.status_code == 302
+    assert "session_id" in response.cookies
+
+
+def test_signup_invalid_email_post_request(client: TestClient):
+    """Test Failed POST Request to signup page from invalid email"""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "testgmail.com",
+            "password": "Abcd123!",
+            "reconfirmPassword": "Abcd123!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "value is not a valid email address: An email address must have an @-sign."
+    )
+
+
+def test_signup_password_length_le8_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to password less than 8 characters."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "Abcd12!",
+            "reconfirmPassword": "Abcd12!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password length should be at least 8 characters but not more than 20 characters"
+    )
+
+
+def test_signup_password_length_gr20_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to password more than 20 characters."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "Abcdefghij1234567890!",
+            "reconfirmPassword": "Abcdefghij1234567890!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password length should be at least 8 characters but not more than 20 characters"
+    )
+
+
+def test_signup_password_no_numbers_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to no numbers in password."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "Abcdefg!",
+            "reconfirmPassword": "Abcdefg!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password should have at least one numeral"
+    )
+
+
+def test_signup_password_no_uppercase_char_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to no uppercase characters in password."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "abcdefg1!",
+            "reconfirmPassword": "abcdefg1!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password should have at least one uppercase letter"
+    )
+
+
+def test_signup_password_no_lowercase_char_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to no lowercase characters in password."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "ABCD123!",
+            "reconfirmPassword": "ABCD123!",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password should have at least one lowercase letter"
+    )
+
+
+def test_signup_password_no_special_char_post_request(client: TestClient):
+    """Test Failed POST Request to signup page due to no special characters in password."""
+    response = client.post(
+        "/signup",
+        data={
+            "username": "test@gmail.com",
+            "password": "Abcd1234",
+            "reconfirmPassword": "Abcd1234",
+        },
+    )
+    response_dict = response.json()
+
+    assert response.status_code == 400
+    assert (
+        response_dict["detail"]
+        == "Value error, Password should have at least one special character"
+    )
